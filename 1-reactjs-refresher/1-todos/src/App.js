af840eb5851fd1e20f3c20229b3cfe747d67586f
igmtink <igmtse@gmail.com>
2023-06-05 16:35:29 +0800

    Todo Component

--- 1-reactjs-refresher/1-todos/src/App.js
+++ 1-reactjs-refresher/1-todos/src/App.js
@@ -1,8 +1,13 @@
+import Todo from "./components/todo";
+
 function App() {
   return (
-    <div className="px-4 py-8">
-      
-    </div>
+    <main className="px-4 py-8">
+      <h1 className="uppercase text-4xl text-center font-bold mb-12">
+        MY TODOS
+      </h1>
+      <Todo />
+    </main>
   );
 }
 
---
+++ 1-reactjs-refresher/1-todos/src/components/todo.js
@@ -0,0 +1,10 @@
+function Todo() {
+    return <div className="p-4 bg-stone-50 rounded-lg shadow-md">
+        <h2 className="uppercase text-lg font-bold">TITLE</h2>
+        <div className="flex justify-end">
+            <button className="px-3 py-2 bg-stone-950 hover:bg-stone-900 transition-colors text-stone-50 rounded-md">Delete</button>
+        </div>
+    </div>
+}
+
+export default Todo
