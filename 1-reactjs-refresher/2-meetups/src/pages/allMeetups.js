13aacf4973489f7a446d92015f7e42417fa226e2
igmtink <igmtse@gmail.com>
2023-06-06 10:57:17 +0800

    Centering H1 All Meetups

--- 1-reactjs-refresher/2-meetups/src/pages/allMeetups.js
+++ 1-reactjs-refresher/2-meetups/src/pages/allMeetups.js
@@ -3,7 +3,7 @@ import MeetupList from "../components/meetups/meetupList";
 function AllMeetupsPage() {
   return (
     <div>
-      <h1 className="text-2xl font-bold uppercase mb-8">All Meetups Page</h1>
+      <h1 className="text-2xl font-bold uppercase mb-8 text-center">All Meetups Page</h1>
       <MeetupList />
     </div>
   );
