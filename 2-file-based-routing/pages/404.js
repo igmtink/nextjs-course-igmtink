a6fb311c52437413e721b3085ab1f8c458dffd4a
igmtink <igmtse@gmail.com>
2023-06-07 16:06:30 +0800

    404

---
+++ 2-file-based-routing/pages/404.js
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center">
+      <h1 className="text-4xl font-bold uppercase">Page not found!</h1>
+    </div>
+  )
+}
