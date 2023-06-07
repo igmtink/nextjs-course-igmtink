70c7bd5e6f6f23638c45ef389412e32121e36776
igmtink <igmtse@gmail.com>
2023-06-07 15:39:15 +0800

    Navigating to Dynamic Routes

--- 2-file-based-routing/pages/clients/index.js
+++ 2-file-based-routing/pages/clients/index.js
@@ -1,7 +1,40 @@
+import Link from 'next/link'
+
 export default function Clients() {
+  const clients = [
+    {
+      id: 'max',
+      name: 'Maximilian'
+    },
+    {
+      id: 'manu',
+      name: 'Manuel'
+    }
+  ]
+
   return (
     <div>
       <h1>Clients</h1>
+
+      {/* NAVIGATING TO DYNAMIC ROUTES */}
+      <ul>
+        <li>
+          <Link href="/clients/max">Maximilian</Link>
+        </li>
+        <li>
+          <Link href="/clients/manu">Manuel</Link>
+        </li>
+      </ul>
+
+      {/* OR */}
+
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
