2f6faa2604a1297f79e6c7b0b700803bbbba02a0
igmtink <igmtse@gmail.com>
2023-06-07 11:55:50 +0800

    Add React Context

--- 1-reactjs-refresher/2-meetups/src/components/layout/mainNavigation.js
+++ 1-reactjs-refresher/2-meetups/src/components/layout/mainNavigation.js
@@ -1,8 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
+
+import { useContext } from 'react'
+import FavoritesContext from '../../store/favorites-context'
 
 function MainNavigation() {
+  const favoritesCtx = useContext(FavoritesContext)
+
   return (
-    
     <header className="p-4 bg-stone-950 text-stone-50">
       <nav className="flex justify-between items-center max-w-4xl mx-auto">
         <h1 className="font-bold text-lg">Meetups</h1>
@@ -13,13 +17,16 @@ function MainNavigation() {
           <li className="hover:text-stone-400 transition-colors">
             <Link to="/new-meetup">New Meetup</Link>
           </li>
-          <li className="hover:text-stone-400 transition-colors">
+          <li className="hover:text-stone-400 transition-colors relative">
             <Link to="/favorites">Favorites</Link>
+            <span className="absolute block top-0 right-0 -translate-y-1 translate-x-2 rounded-[50%] w-4 h-4 text-xs text-center bg-cyan-500">
+              {favoritesCtx.totalFavorites}
+            </span>
           </li>
         </ul>
       </nav>
     </header>
-  );
+  )
 }
 
-export default MainNavigation;
+export default MainNavigation
