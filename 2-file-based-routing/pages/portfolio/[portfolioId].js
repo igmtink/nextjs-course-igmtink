2b24d4bc3fbff7c63a4d56597a5e01f02e7048dc
igmtink <igmtse@gmail.com>
2023-06-07 14:20:52 +0800

    Extract Dynamic Path Segment Data (Dynamic Routes)

--- 2-file-based-routing/pages/portfolio/[portfolioId].js
+++ 2-file-based-routing/pages/portfolio/[portfolioId].js
@@ -1,5 +1,12 @@
+import { useRouter } from 'next/router'
+
 // DYNAMIC ROUTES
 export default function PortfolioId() {
+  const router = useRouter()
+
+  // Extract Dynamic Path Segment Data (Dynamic Routes)
+  console.log(router.query.portfolioId)
+
   return (
     <div>
       <h1>Portfolio Id</h1>
