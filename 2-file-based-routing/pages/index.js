6e5b1afefd06df797dafffa9f1d4254d3c41af44
igmtink <igmtse@gmail.com>
2023-06-07 15:24:29 +0800

    Navigating with the Link

--- 2-file-based-routing/pages/index.js
+++ 2-file-based-routing/pages/index.js
@@ -1,7 +1,19 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <main>
       <h1>Home</h1>
+
+      {/* NAVIGATING WITH THE LINKS */}
+      <ul>
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link href="/clients">Clients</Link>
+        </li>
+      </ul>
     </main>
   )
 }
