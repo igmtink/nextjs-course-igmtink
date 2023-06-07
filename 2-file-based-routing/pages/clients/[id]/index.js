128ba0e936903ef850ec66eded021e82f1ae8937
igmtink <igmtse@gmail.com>
2023-06-07 15:55:48 +0800

    Navigating Programmatically

--- 2-file-based-routing/pages/clients/[id]/index.js
+++ 2-file-based-routing/pages/clients/[id]/index.js
@@ -1,7 +1,25 @@
+import { useRouter } from 'next/router'
+
 export default function ClientProjects() {
+  // NAVIGATING PROGRAMMATICALLY
+  const router = useRouter()
+
+  const loadProjectHandler = () => {
+    router.push('/clients/max/projecta')
+
+    // OR
+
+    router.push({
+      pathname: '/clients/[id]/[clientprojectid]',
+      query: { id: 'max', clientprojectid: 'projecta' }
+    })
+  }
+
   return (
     <div>
       <h1>Client Projects</h1>
+
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   )
 }
