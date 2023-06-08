fcc698962d8aa517b375c145bd78ee7ff7332b83
igmtink <igmtse@gmail.com>
2023-06-08 13:58:28 +0800

    Styled Event List

--- 3-project-file-based-routing/components/events/event-item.js
+++ 3-project-file-based-routing/components/events/event-item.js
@@ -1,11 +1,25 @@
-export default function EventItem({
-  id,
-  title,
-  description,
-  location,
-  date,
-  image,
-  isFeatured
-}) {
-  return <li></li>
+import Link from 'next/link'
+
+export default function EventItem({ id, title, date, location, image }) {
+  const formattedDate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+  })
+  const formattedAddress = location.replace(', ', '\n')
+  const exploreLink = `/events/${id}`
+
+  return (
+    <li>
+      <img src={`/${image}`} alt={title} />
+      <div>
+        <h2>{title}</h2>
+        <time>{formattedDate}</time>
+        <address>{formattedAddress}</address>
+      </div>
+      <div>
+        <Link href={exploreLink}>Explore Event</Link>
+      </div>
+    </li>
+  )
 }
