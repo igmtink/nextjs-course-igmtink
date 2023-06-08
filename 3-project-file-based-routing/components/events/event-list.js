e2549e3be73d7349a33aaa18e701994fb8d4f142
igmtink <igmtse@gmail.com>
2023-06-08 13:57:22 +0800

    Revert "Styled Home Page"
    This reverts commit 1e679f1d85784e0d3ef1467270c92155467d5bb1.

--- 3-project-file-based-routing/components/events/event-list.js
+++ 3-project-file-based-routing/components/events/event-list.js
@@ -4,14 +4,7 @@ export default function EventList({ items }) {
   return (
     <ul>
       {items.map((event) => (
-        <EventItem
-          key={event.id}
-          id={event.id}
-          image={event.image}
-          title={event.title}
-          date={event.date}
-          location={event.location}
-        />
+        <EventItem key={event.id} />
       ))}
     </ul>
   )
--- 3-project-file-based-routing/pages/index.js
+++ 3-project-file-based-routing/pages/index.js
@@ -1,11 +1,10 @@
-import EventList from '@/components/events/event-list'
 import { getFeaturedEvents } from '@/dummy-data'
 
 export default function Home() {
-  const featuredEvents = getFeaturedEvents()
+  const featuredEvents = getFeaturedEvents
   return (
-    <main className="p-4 max-w-lg mx-auto">
-      <EventList items={featuredEvents} />
+    <main>
+      <h1>Home</h1>
     </main>
   )
 }
