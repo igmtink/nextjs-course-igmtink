d4e41a53aca1236156d5bf70edac243d0e5b3a7c
igmtink <igmtse@gmail.com>
2023-06-08 15:36:38 +0800

    Added Icon

--- 3-project-file-based-routing/components/events/event-item.js
+++ 3-project-file-based-routing/components/events/event-item.js
@@ -20,7 +20,23 @@ export default function EventItem({ id, title, date, location, image }) {
           <address className="whitespace-pre">{formattedAddress}</address>
         </div>
         <div className="flex justify-end">
-          <Button link={exploreLink}>Explore Event</Button>
+          <Button link={exploreLink}>
+            Explore Event{' '}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+              />
+            </svg>
+          </Button>
         </div>
       </div>
     </Card>
