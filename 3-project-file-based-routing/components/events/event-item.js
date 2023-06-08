6c86e2739ac174fc65c39bf4c91307002500151f
igmtink <igmtse@gmail.com>
2023-06-08 15:45:22 +0800

    Added Icons

--- 3-project-file-based-routing/components/events/event-item.js
+++ 3-project-file-based-routing/components/events/event-item.js
@@ -1,3 +1,6 @@
+import ArrowRight from '../icons/arrow-icon'
+import DateIcon from '../icons/date-icon'
+import LocationIcon from '../icons/location-icon'
 import { Button, Card } from '../ui/igmtink'
 
 export default function EventItem({ id, title, date, location, image }) {
@@ -15,11 +18,20 @@ export default function EventItem({ id, title, date, location, image }) {
       <div className="px-6 py-4 w-full">
         <div className="mb-6">
           <h2 className="text-lg font-bold uppercase mb-2">{title}</h2>
-          <time className="mb-2 block font-medium">{formattedDate}</time>
-          <address className="whitespace-pre">{formattedAddress}</address>
+          <div className="flex gap-2 items-center">
+            <DateIcon />
+            <time className="mb-2 block font-medium">{formattedDate}</time>
+          </div>
+          <div className="flex gap-2 items-center">
+            <LocationIcon />
+            <address className="whitespace-pre">{formattedAddress}</address>
+          </div>
         </div>
         <div className="flex justify-end">
-          <Button link={exploreLink}>Explore Event</Button>
+          <Button link={exploreLink}>
+            Explore Event
+            <ArrowRight />
+          </Button>
         </div>
       </div>
     </Card>
