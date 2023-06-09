2515ff47bf109ec3a9fb892ca1fab157efc80b10
igmtink <igmtse@gmail.com>
2023-06-09 13:41:11 +0800

    Styled Button Tweaked

--- 3-project-file-based-routing/components/ui/igmtink.js
+++ 3-project-file-based-routing/components/ui/igmtink.js
@@ -1,13 +1,24 @@
 import Link from 'next/link'
 
-export const Button = ({ children, link }) => {
+export const Button = ({ children, link, onClick }) => {
+  if (link) {
+    return (
+      <Link
+        className="flex gap-2 items-center w-fit px-4 py-3 bg-teal-500 hover:bg-teal-600 transition-colors rounded-md text-white"
+        href={link}
+      >
+        {children}
+      </Link>
+    )
+  }
+
   return (
-    <Link
+    <button
       className="flex gap-2 items-center w-fit px-4 py-3 bg-teal-500 hover:bg-teal-600 transition-colors rounded-md text-white"
-      href={link}
+      onClick={onClick}
     >
       {children}
-    </Link>
+    </button>
   )
 }
 
