a672df1a9d2ea5174c43cb64be8247a72486c295
igmtink <igmtse@gmail.com>
2023-06-09 13:42:47 +0800

    Added Events Filter

--- 3-project-file-based-routing/pages/events/index.js
+++ 3-project-file-based-routing/pages/events/index.js
@@ -1,10 +1,15 @@
 import EventList from '@/components/events/event-list'
+import EventsSearch from '@/components/events/events-search'
 import { getAllEvents } from '@/dummy-data'
 
 export default function Events() {
   const events = getAllEvents()
   return (
     <div className="p-4 pt-20">
+      <div className="mb-4">
+        <EventsSearch />
+      </div>
+
       <EventList items={events} />
     </div>
   )
