8796b433d5bf17891b629a36bd24eb1f9cd45846
igmtink <igmtse@gmail.com>
2023-06-09 14:28:37 +0800

    Added another Events Filter

--- 3-project-file-based-routing/pages/events/index.js
+++ 3-project-file-based-routing/pages/events/index.js
@@ -2,7 +2,21 @@ import EventList from '@/components/events/event-list'
 import EventsSearch from '@/components/events/events-search'
 import { getAllEvents } from '@/dummy-data'
 
-const EVENTS_FILTER = ['2021', '2022', '2023']
+const EVENTS_FILTER_YEAR = ['2021', '2022', '2023']
+const EVENTS_FILTER_MONTH = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+]
 
 export default function Events() {
   const events = getAllEvents()
@@ -11,11 +25,11 @@ export default function Events() {
       <div className="grid grid-cols-2 items-center mb-4">
         <div className="flex gap-2 items-center">
           <label>Year</label>
-          <EventsSearch options={EVENTS_FILTER} select="Select Year" />
+          <EventsSearch options={EVENTS_FILTER_YEAR} select="Select Year" />
         </div>
         <div className="flex gap-2 items-center">
           <label>Month</label>
-          <EventsSearch options={EVENTS_FILTER} select="Select Month" />
+          <EventsSearch options={EVENTS_FILTER_MONTH} select="Select Month" />
         </div>
       </div>
 
