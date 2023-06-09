3d98387c25f8160fb37a27f08c2fe68c876ef21f
igmtink <igmtse@gmail.com>
2023-06-09 14:26:05 +0800

    Added another Events Filter

--- 3-project-file-based-routing/pages/events/index.js
+++ 3-project-file-based-routing/pages/events/index.js
@@ -8,8 +8,15 @@ export default function Events() {
   const events = getAllEvents()
   return (
     <div className="p-4 pt-20">
-      <div className="mb-4">
-        <EventsSearch options={EVENTS_FILTER} />
+      <div className="grid grid-cols-2 items-center mb-4">
+        <div className="flex gap-2 items-center">
+          <label>Year</label>
+          <EventsSearch options={EVENTS_FILTER} select="Select Year" />
+        </div>
+        <div className="flex gap-2 items-center">
+          <label>Month</label>
+          <EventsSearch options={EVENTS_FILTER} select="Select Month" />
+        </div>
       </div>
 
       <EventList items={events} />
