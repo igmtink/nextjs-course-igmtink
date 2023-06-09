5fdebbcc2f961e47fe4206d5c5d4a6687013358b
igmtink <igmtse@gmail.com>
2023-06-09 10:01:29 +0800

    Added Navigation Component

---
+++ 3-project-file-based-routing/components/layout/navigation.js
@@ -0,0 +1,9 @@
+export default function Navigation() {
+  return (
+    <header className="shadow-lg bg-teal-500 text-white fixed inset-x-0">
+      <nav className="p-4">
+        <h1 className="uppercase font-bold text-lg">Occasion</h1>
+      </nav>
+    </header>
+  )
+}
--- 3-project-file-based-routing/pages/_app.js
+++ 3-project-file-based-routing/pages/_app.js
@@ -1,5 +1,11 @@
+import Navigation from '@/components/layout/navigation'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navigation />
+      <Component {...pageProps} />
+    </>
+  )
 }
