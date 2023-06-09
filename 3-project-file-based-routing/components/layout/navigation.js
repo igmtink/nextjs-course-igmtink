1777e3d8ee146f00855d26cda86edd958c447b2a
igmtink <igmtse@gmail.com>
2023-06-09 10:56:45 +0800

    Added Navigation Link

--- 3-project-file-based-routing/components/layout/navigation.js
+++ 3-project-file-based-routing/components/layout/navigation.js
@@ -1,8 +1,18 @@
+import Link from 'next/link'
+
 export default function Navigation() {
   return (
     <header className="shadow-lg bg-teal-500 text-white fixed inset-x-0">
-      <nav className="p-4">
-        <h1 className="uppercase font-bold text-lg">Occasion</h1>
+      <nav className="p-4 flex justify-between items-center">
+        <Link href="/" className="uppercase font-bold text-lg">
+          Occasion
+        </Link>
+
+        <ul className="flex items-center gap-4">
+          <li>
+            <Link href="/events">Events</Link>
+          </li>
+        </ul>
       </nav>
     </header>
   )
