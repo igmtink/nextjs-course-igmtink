35d547d12774d00df97b460d6ab79a0b28e4fb2f
igmtink <igmtse@gmail.com>
2023-06-11 21:12:38 +0800

    Added getStaticProps for Server-side Code & Using the File System

---
+++ 4-pre-rendering-and-data-fetching/pages/index.js
@@ -0,0 +1,27 @@
+import path from 'path'
+import fs from 'fs/promises'
+
+export default function Home({ products }) {
+  return (
+    <ul>
+      {products.map(product => (
+        <li key={product.id}>{product.title}</li>
+      ))}
+    </ul>
+  )
+}
+
+export async function getStaticProps() {
+  // path.join(process.cwd()) to get our current working directory (root folder)
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json')
+  // fs.readFile() to read inside our file
+  const jsonData = await fs.readFile(filePath)
+  // return an Object
+  const data = JSON.parse(jsonData)
+
+  return {
+    props: {
+      products: data.products
+    }
+  }
+}
